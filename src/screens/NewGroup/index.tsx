622529cdc89c96780e4c60c9cc30835368d0a032
marlisonmourao <bentesmourao@gmail.com>
2022-10-08 18:28:04 -0400

    feat:  tela de NewGroup

---
+++ src/screens/NewGroup/index.tsx
@@ -0,0 +1,34 @@
+import { Container, Content, Icon } from "./styles";
+
+
+import { Header } from "@components/Header";
+import { HighLight } from "@components/HighLight";
+import { Button } from "@components/Button";
+import { Input } from "@components/Input";
+
+export function NewGroup() {
+  return (
+    <Container>
+      <Header showBackButton />
+
+      <Content>
+        <Icon />
+        
+        <HighLight
+          title="Nova turma"
+          subtitle="crie a turma para adicionar as pessoas"
+        />
+
+        <Input
+          placeholder="Nome da turma"
+          
+        />
+
+        <Button 
+          title="Criar"
+          style={{ marginTop: 20 }}  
+        />
+      </Content>
+    </Container>
+  );
+}
