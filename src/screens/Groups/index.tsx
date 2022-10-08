80891cf52eeee083f112158891186fee50ce397b
marlisonmourao <bentesmourao@gmail.com>
2022-10-08 18:26:11 -0400

    feat: tela de Groups

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import { FlatList } from "react-native";
+import { Container } from "./styles";
+
+import { Header } from "@components/Header";
+import { HighLight } from "@components/HighLight";
+import { GroupCard } from "@components/GroupCard";
+import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
+
+export function Groups() {
+  const [groups, setGroups] = useState<string[]>([]);
+
+  return (
+    <Container>
+      <Header />
+
+      <HighLight 
+        title="Turmas"
+        subtitle="jogue com sua turma"
+      />
+
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard title={item}/>
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" /> 
+        )}
+      />
+
+      <Button 
+        title="Criar nova turma"
+      />
+    </Container>
+  );
+}
